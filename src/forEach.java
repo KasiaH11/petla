3116e0146a7ae1aa3953cfb14c9d15cc6a99fceb
KasiaH11 <katarzyna.haladyniak.praca@onet.pl>
2022-11-23 19:24:12 +0100

    użcie petli foreach do imion kotw

---
+++ src/forEach.java
@@ -0,0 +1,10 @@
+public class forEach {
+    public static void main(String[] args) {
+        String[] cats = new String[]{"Burek", "Mruczus", "Iskierka", "Klakier"};
+        for (String cat:cats){
+            System.out.println(cat);
+
+        }
+
+    }
+}
